6fa425fe646d20e6dfe96e957bae5f407769b401
ladni <ladnik@hotmail.com>
2020-07-10 16:38:40 +0300

    interceptor added

--- src/controllers/cat.controller.ts
+++ src/controllers/cat.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Req, Res, Body, HttpStatus, UsePipes } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, Body, HttpStatus, UsePipes, UseInterceptors } from '@nestjs/common';
 import { CatService } from '../providers/cat.service';
 import { Request, Response } from 'express';
 import { CatDto } from '../dto/cat.dto';
 import { ValidationPipe } from '../pipes/validation.pipe';
+import { LoggingInterceptor } from '../interceptors/cat.interceptor';
 
+@UseInterceptors(new LoggingInterceptor())
 @Controller('cat')
 export class CatController {
   constructor(private readonly catService: CatService) {}
